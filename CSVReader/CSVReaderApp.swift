86996789781cda9484781ea75a87e67b0e5d16ba
Thomas Leonhardt <t.leonhardt@gmx.net>
2022-04-10 08:55:27 +0200

    Initial Commit

---
+++ CSVReader/CSVReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSVReaderApp.swift
+//  CSVReader
+//
+//  Created by Thomas (privat) Leonhardt on 10.04.22.
+//
+
+import SwiftUI
+
+@main
+struct CSVReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

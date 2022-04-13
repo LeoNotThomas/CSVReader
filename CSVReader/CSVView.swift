ac16162ed084461cfa5366bc615c93023882a310
Thomas Leonhardt <t.leonhardt@gmx.net>
2022-04-13 19:56:47 +0200

    implement paging

--- CSVReader/CSVView.swift
+++ CSVReader/CSVView.swift
@@ -9,17 +9,44 @@ import SwiftUI
 
 struct CSVView: View {
     @State private var csvData = CSVDataSource().data
+    private let pageEntrys = 3
+    var page: Int
     var body: some View {
         Form {
-            ForEach(csvData!.rows, id: \.self) { csvRow in
-//                let array: [String] = row.row
-                HStack(alignment: .bottom) {
+            let rows = getPage()
+            ForEach(rows, id: \.self) { csvRow in
+                HStack(alignment: .bottom, spacing: 10) {
                     ForEach(csvRow.row, id: \.self) { text in
                         Text(text)
+                            .frame(width: 100,  alignment: .leading)
+                            .background(.red)
                     }
                 }
             }
+            if rows.last != csvData?.rows.last {
+                NavigationLink(destination: CSVView(page: page + 1)) {
+                Text("Choose Tails")
+                }
+            }
         }
+        .navigationTitle("Page: \(page)")
+    }
+    
+    private func getPage() -> [CSVRow] {
+        var rows = [CSVRow]()
+        for i in (page * pageEntrys)...(page * pageEntrys) + pageEntrys - 1 {
+            if let row = csvData?.rows[i] {
+                rows.append(row)
+                if row == csvData?.rows.last {
+                    break
+                }
+            }
+        }
+        return rows
+    }
+    
+    init(page: Int = 0) {
+        self.page = page
     }
 }
 
---
+++ CSVReader/MainView.swift
@@ -0,0 +1,23 @@
+//
+//  MainView.swift
+//  CSVReader
+//
+//  Created by Thomas (privat) Leonhardt on 13.04.22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        NavigationView {
+            CSVView()
+        }
+        
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
